e52a90bdbcbcbef6b27d9f08f8140870e7c123b6
master <hulktv222@mail.ru>
2020-10-13 16:27:50 +0300

    test commit

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 
 headers = requests.utils.default_headers()
 headers.update({'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/69.0'})
+
 url = "https://mtuci.ru/time-table/"
 req = requests.get(url, headers)
 soup = BeautifulSoup(req.content, 'html.parser')
