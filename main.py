ad1078f97f180739879a5d60040e7fdcb02ac6c0
master <hulktv222@mail.ru>
2020-10-13 16:24:35 +0300

    first commit

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from bs4 import BeautifulSoup
 
 headers = requests.utils.default_headers()
 headers.update({'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/69.0'})
-
 url = "https://mtuci.ru/time-table/"
 req = requests.get(url, headers)
 soup = BeautifulSoup(req.content, 'html.parser')
@@ -22,4 +21,3 @@ for link in all_links:
             f.write(ufr.content)  # записываем содержимое в файл; как видите - content запроса
             f.close()
 print(excel_links)
-
